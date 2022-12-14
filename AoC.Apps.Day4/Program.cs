39df35ca90ccb4d8ed136a4784db18370d81af3c
Mark ter Luun <markterluun@gmail.com>
2022-12-15 00:17:25 +0100

    Add project files

---
+++ AoC.Apps.Day4/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace AoC.Apps.Day4;
+
+static class Program
+{
+    static void Main() {
+        var input = File.ReadAllLines("input.txt");
+        var testInput = File.ReadAllLines("input_test.txt");
+    }
+}
