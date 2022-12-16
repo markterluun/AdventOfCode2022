7b75c1ebb02ffd7530e1f9d8b62afbc789cbfdcb
Mark <markterluun@gmail.com>
2022-12-16 11:29:26 +0100

    Add project files

---
+++ AoC.Apps.Day6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AoC.Apps.Day6;
+
+static class Program
+{
+    static void Main()
+    {
+        var input = File.ReadAllLines(".input.txt");
+        var testInput = File.ReadAllLines(".testinput.txt");
+    }
+}
