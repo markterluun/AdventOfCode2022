614456f69ff42a19722b097389fdba3dd2dee7c4
Mark ter Luun <markterluun@gmail.com>
2022-12-17 18:06:21 +0100

    Part 1

--- AoC.Apps.Day7/Program.cs
+++ AoC.Apps.Day7/Program.cs
@@ -1,4 +1,5 @@
 ﻿using AoC.Apps.Day7.CLI;
+using AoC.Apps.Day7.FileSystem;
 
 namespace AoC.Apps.Day7;
 
@@ -8,18 +9,32 @@ static class Program
         var input = File.ReadAllLines(".input.txt");
         var testInput = File.ReadAllLines(".testinput.txt");
 
-        Part1(testInput);
+        Part1(input);
     }
 
     static void Part1(string[] input) {
         var interpreter = new Interpreter();
-
         foreach (var line in input) {
             interpreter.Parse(line);
         }
 
-        interpreter.State.PrintDirectoryListing();
-        Console.WriteLine();
-        Console.WriteLine($"Total size: {interpreter.State.FSRoot.GetSize()}");
+        // Find directories of size <= 100.000
+        var maxSize = 100000;
+        var smallDirectories = FindSmallDirectories(interpreter.State.FSRoot, maxSize);
+        var totalSize = smallDirectories.Sum(d => d.GetSize());
+
+        Console.WriteLine(totalSize);
+    }
+
+    static IEnumerable<Node> FindSmallDirectories(Node root, int maxSize) {
+        foreach (var node in root.Nodes) {
+            if (node.GetSize() <= maxSize) {
+                yield return node;
+            }
+
+            foreach (var subNode in FindSmallDirectories(node, maxSize)) {
+                yield return subNode;
+            }
+        }
     }
 }
